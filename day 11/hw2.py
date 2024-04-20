a353a9bd9afb200e572e3deac50fd7ece25aa52d
iakobdoghonadze <166760138+iakobdoghonadze@users.noreply.github.com>
2024-04-20 12:51:08 +0400

    Add files via upload

---
+++ day 11/hw2.py
@@ -0,0 +1,15 @@
+#3 დავალება
+#გააკეთეთ 10 მაგალითი ლოგიკურ ოპერატორებზე(and, or)
+
+
+print(True and False)
+print(False and True)
+print(True and True and False)
+print(False and False)
+print(True and True)
+print(False and True)
+print(True or False)
+print(False or True)
+print(False or False or True)
+print(True or False and True)
+print(True or False or False or True)
